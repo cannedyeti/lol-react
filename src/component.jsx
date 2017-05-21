eedee1335927873a6b3b8f8beb1d7b18ab778d03
CannedYeti <cpotebnya@gmail.com>
2017-05-21 12:16:45 -0700

    initial

---
+++ src/component.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class MyComponent extends Component {
+  render () {
+    return (
+      <div className="well">
+        <h1>Hello</h1>
+        <p>This is my first component. It contains HTML!</p>
+      </div>
+    );
+  }
+}
+
+module.exports = MyComponent;
